5ee70b4f9197fc93360323820417b60cb9cfc0ab
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-23 17:03:16 -0500

    Create CurrencyOperationNonZero.cs

---
+++ source/Effects/CurrencyOperationNonZero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Strategies;
+using Strategies.Effects;
+
+namespace Strategia
+{
+    /// <summary>
+    /// Special CurrencyOperation that only acts on non-zero values.
+    /// </summary>
+    public class CurrencyOperationNonZero : CurrencyOperation
+    {
+        public CurrencyOperationNonZero(Strategy parent)
+            : base(parent)
+        {
+        }
+
+        public CurrencyOperationNonZero(Strategy parent, float minValue, float maxValue, Currency currency, CurrencyOperation.Operator op, TransactionReasons AffectReasons, string description)
+            : base(parent, minValue, maxValue, currency, op, AffectReasons, description)
+        {
+        }
+
+        protected override void OnLoadFromConfig(ConfigNode node)
+        {
+            base.OnLoadFromConfig(node);
+        }
+
+        protected override void OnEffectQuery(CurrencyModifierQuery qry)
+        {
+            // Check if it's non-zero
+            if (Math.Abs(qry.GetInput(Currency.Funds)) < 0.01 && Math.Abs(qry.GetInput(Currency.Science)) < 0.01 && Math.Abs(qry.GetInput(Currency.Reputation)) < 0.01)
+            {
+                return;
+            }
+
+            base.OnEffectQuery(qry);
+        }
+    }
+}
