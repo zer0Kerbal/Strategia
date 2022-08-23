5041541acf5a43a649107ccf3987ed24899cf3ff
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-23 17:07:35 -0500

    Create Generic.cs

---
+++ source/Strategy/Generic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Strategies;
+
+namespace Strategia
+{
+    public class AstronautTrainingProgram1 : UpgradeableStrategy { }
+    public class AstronautTrainingProgram2 : UpgradeableStrategy { }
+    public class AstronautTrainingProgram3 : UpgradeableStrategy { }
+    public class SlotMachine1 : UpgradeableStrategy { }
+    public class SlotMachine2 : UpgradeableStrategy { }
+    public class SlotMachine3 : UpgradeableStrategy { }
+    public class TechStagnation1 : UpgradeableStrategy { }
+    public class TechStagnation2 : UpgradeableStrategy { }
+    public class TechStagnation3 : UpgradeableStrategy { }
+    public class EngineerFocus1 : UpgradeableStrategy { }
+    public class EngineerFocus2 : UpgradeableStrategy { }
+    public class EngineerFocus3 : UpgradeableStrategy { }
+    public class PilotFocus1 : UpgradeableStrategy { }
+    public class PilotFocus2 : UpgradeableStrategy { }
+    public class PilotFocus3 : UpgradeableStrategy { }
+    public class ScientistFocus1 : UpgradeableStrategy { }
+    public class ScientistFocus2 : UpgradeableStrategy { }
+    public class ScientistFocus3 : UpgradeableStrategy { }
+    public class ImpactorProbes : UpgradeableStrategy { }
+    public class FlyByProbes : UpgradeableStrategy { }
+    public class BoldlyGo1 : UpgradeableStrategy { }
+    public class BoldlyGo2 : UpgradeableStrategy { }
+    public class BoldlyGo3 : UpgradeableStrategy { }
+    public class ProbeFrenzy1 : UpgradeableStrategy { }
+    public class ProbeFrenzy2 : UpgradeableStrategy { }
+    public class ProbeFrenzy3 : UpgradeableStrategy { }
+    public class MediaCircus1 : UpgradeableStrategy { }
+    public class MediaCircus2 : UpgradeableStrategy { }
+    public class MediaCircus3 : UpgradeableStrategy { }
+    public class MassiveLaunches1 : UpgradeableStrategy { }
+    public class MassiveLaunches2 : UpgradeableStrategy { }
+    public class MassiveLaunches3 : UpgradeableStrategy { }
+    public class LocalScience1 : UpgradeableStrategy { }
+    public class LocalScience2 : UpgradeableStrategy { }
+    public class LocalScience3 : UpgradeableStrategy { }
+    public class FreeIceCream1 : UpgradeableStrategy { }
+    public class FreeIceCream2 : UpgradeableStrategy { }
+    public class FreeIceCream3 : UpgradeableStrategy { }
+}
