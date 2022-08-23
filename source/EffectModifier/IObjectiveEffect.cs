58b3bb260ffa8db1f437d21f3e22c6d0e74eebb7
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-23 17:02:31 -0500

    Create IObjectiveEffect.cs

---
+++ source/EffectModifier/IObjectiveEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Strategies;
+using Strategies.Effects;
+
+namespace Strategia
+{
+    public interface IObjectiveEffect
+    {
+        IEnumerable<string> ObjectiveText();
+
+        double rewardFunds { get; }
+        float rewardScience { get; }
+        float rewardReputation { get; }
+        double failureFunds { get; }
+        float failureScience { get; }
+        float failureReputation { get; }
+        double advanceFunds { get; }
+        float advanceScience { get; }
+        float advanceReputation { get; }
+    }
+}
