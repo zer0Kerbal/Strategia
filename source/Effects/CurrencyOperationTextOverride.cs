773b10f8d6e3d9297ac7525503f876a666ddccad
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-23 17:03:26 -0500

    Create CurrencyOperationTextOverride.cs

---
+++ source/Effects/CurrencyOperationTextOverride.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Strategies;
+using Strategies.Effects;
+using ContractConfigurator;
+
+namespace Strategia
+{
+    /// <summary>
+    /// Special CurrencyOperation that overrides the text.
+    /// </summary>
+    public class CurrencyOperationTextOverride : CurrencyOperationWithPopup
+    {
+        string description;
+
+        public CurrencyOperationTextOverride(Strategy parent)
+            : base(parent)
+        {
+        }
+
+        protected override string GetDescription()
+        {
+            return description;
+        }
+
+        protected override void OnLoadFromConfig(ConfigNode node)
+        {
+            base.OnLoadFromConfig(node);
+
+            description = ConfigNodeUtil.ParseValue<string>(node, "description");
+        }
+    }
+}
