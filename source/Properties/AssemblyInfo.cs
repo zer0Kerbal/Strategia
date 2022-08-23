2b52994b2ffc3915601882abec924306b91feb88
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-23 17:04:02 -0500

    Create AssemblyInfo.cs

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Strategia")]
+[assembly: AssemblyDescription("Strategia - A Brand New Strategy System! For Kerbal Space Program")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Strategia")]
+[assembly: AssemblyCopyright("Copyright © 2015 Jeremie Rossignol (aka nightingale); 2022 © zer0Kerbal")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6c97f8ad-9355-4517-8a41-866d51f219e9")]
+
+//[assembly: AssemblyVersion("1.0")]
+//[assembly: AssemblyFileVersion("1.8.0")]
+//[assembly: AssemblyInformationalVersion("1.8.0")]
+//[assembly: KSPAssembly("Strategia", 1, 0)]
