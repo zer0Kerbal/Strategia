f9f4c755a148eb15165ce751903b0067bb42be7e
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-23 17:07:54 -0500

    Create StringUtil.cs

---
+++ source/Util/StringUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSP;
+
+namespace Strategia
+{
+    public static class StringUtil
+    {
+        static string[] romanNumerals = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV" };
+        static char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+        public static string IntegerToRoman(int num)
+        {
+            return num < romanNumerals.Count() ? romanNumerals[num - 1] : num.ToString();
+        }
+
+        public static string ATrait(string trait)
+        {
+            return (vowels.Contains(trait.ToLower().First()) ? "an " : "a ") + trait.ToLower();
+        }
+    }
+}
